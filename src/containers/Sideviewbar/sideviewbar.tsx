2513b79a6afebbdbbe9d64d87c998e2cf1771d12
sowmithri2001 <sowmi3b@gmail.com>
2023-08-20 10:47:35 +0530

    web_task

--- src/containers/Sideviewbar/sideviewbar.tsx
+++ src/containers/Sideviewbar/sideviewbar.tsx
@@ -2,10 +2,8 @@ import Sidebar from '../Sidebar/Sidebar'
 import Navigator from '../Navigator/Navigator'
 import classes from './Sideviewbar.module.scss'
 
-const sideviewbar = (props: any) => {
-  const sendData = (ele: any) => {
-    props.getData(ele)
-  }
+const sideviewbar = () => {
+
   return (
     <div className={classes.wrapper}>
       <Navigator />
